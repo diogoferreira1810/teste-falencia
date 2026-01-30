d18c6b8646a3f62e44801d16b8e332a8c9e28a2f
diogoferreira1810 <diogo.ferreira18@ua.pt>
2026-01-30 21:16:53 +0000

    feat: integrate Tailwind CSS and update dependencies
    - Added Tailwind CSS and related dependencies to package.json
    - Imported Tailwind CSS in App.css
    - Updated App.tsx to utilize Tailwind CSS for styling and added content about important technologies
    - Configured Vite to use Tailwind CSS plugin

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div className='flex flex-col items-center justify-center min-h-screen text-white'>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <h1 className='text-left text-md mb-4'>Tecnologias importantes para o site</h1>
+          <p>vou fazer uma listagem (kinda) de tecnologias que é importante usarmos no site e também para leres, informares-te e depois testar/inventar para te familiarizares.</p>
+        </div> 
+        <div className='p-8 mt-2 mb-2 border rounded-lg bg-gray-800 w-full'>
+          <h2 className='text-left text-2xl font-bold'>React-Router</h2>
+          <p className='text-left'>React usa a lógica de componentes no seu development, facilitando a criação de interfaces dinâmicas. Quando se clica num link, não está a abrir uma pagina nova, mas sim a renderizar um novo componente. React-router é usado para definir as rotas e a navegação entre esses componentes.</p>
+        </div>
+        <div className='p-8 mt-2 mb-2 border rounded-lg bg-gray-800 w-full'>
+          <h2 className='text-left text-2xl font-bold'>Tailwind</h2>
+          <p className='text-left'>Biblioteca de CSS, usada para estilização mais rápida em vez de escrever CSS tradicional. Usas classes ja definidas que podes combinar para customizar</p>
+        </div>
+        <div className='p-8 mt-2 mb-2 border rounded-lg bg-gray-800 w-full'>
+          <h2 className='text-left text-2xl font-bold'>Stlyed Components</h2>
+          <p className='text-left'>Aqui usas para criar componentes com um estilo ja predefinido. Exemplo - crias um botão base e sempre que precisares de um botão ele já vem com o estilo que tu queres, em vez de teres que meter as classes. Usado em conjunto com tailwind acelera-te imenso o desenvolvimento de um site, quase que é juntar peças para formar o todo</p>
+        </div>
+                <p>
+          Edit no app.tsx
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
